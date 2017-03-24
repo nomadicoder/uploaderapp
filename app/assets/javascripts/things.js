3df3414866b52960ee1c88edf502a9344f18a25d
Steven Ng <steven@nomadicoder.com>
2017-03-24 11:59:01 -0400

    Add JQuery Uploader

---
+++ app/assets/javascripts/things.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('#thing_fileupload').fileupload({
+        dataType: 'script',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo(document.body);
+            });
+        },
+        progressall: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            $('#progress .bar').css(
+                'width',
+                progress + '%'
+            );
+        }
+    });
+
+});
